78ef0a9ba6187d01b0b4bba6649d94dd5df3051e
israelAliyev <space.y@inbox.ru>
2021-05-28 16:06:50 +0400

    i completed based footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -10,6 +10,7 @@ class Footer extends Component {
                 <footer className="footer">
                     <div className="container">
                         <div className="row">
+
                             <div className="footer-col">
                                 <h4>company</h4>
                                 <ul>
@@ -38,8 +39,25 @@ class Footer extends Component {
                                     <li><a href="#">dress</a></li>
                                 </ul>
                             </div>
+
+                            <div className="footer-col footer-contact-us">
+                                <h4>contact us</h4>
+                                <ul>
+                                    <li>Email:</li>
+                                    <li><input size="27" className="rounded-2 footer-contact-us-email-form"
+                                               type="text"/></li>
+                                    <li>Message:</li>
+                                    <li><textarea rows="3" className="rounded-2 footer-contact-us-message-form w-100"/></li>
+                                    <li>
+                                        <button className="btn btn-danger mt-2">Send</button>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+                        <hr className="text-white"/>
+                        <div className="d-flex justify-content-around">
                             <div className="footer-col">
-                                <h4>follow us</h4>
                                 <div className="social-links">
                                     <a href="#"><i className="fab fa-facebook-f"></i></a>
                                     <a href="#"><i className="fab fa-twitter"></i></a>
@@ -47,7 +65,9 @@ class Footer extends Component {
                                     <a href="#"><i className="fab fa-linkedin-in"></i></a>
                                 </div>
                             </div>
+
                         </div>
+
                     </div>
                 </footer>
 
